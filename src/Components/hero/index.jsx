b9f10041a9e1992f85b5463252c28f3eaa85a0d6
nandani <reach.nandanipaliwal@gmail.com>
2023-12-01 11:29:51 +0530

    fix: padding of hero section

--- src/Components/hero/index.jsx
+++ src/Components/hero/index.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className=" relative flex flex-col justify-center items-center -mt-28 overflow-hidden pt-10 pb-16 w-full dark:bg-gray-900 bg-gray-10">
       <main className="w-full">
-        <div className="0 mx-auto max-w-7xl pt-48 sm:pt-36 lg:pb- lg:overflow-hidden">
+        <div className="0 mx-auto max-w-7xl sm:pt-28  lg:overflow-hidden">
           <div className="lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:gap-8">
               <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
