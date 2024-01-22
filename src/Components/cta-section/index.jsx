362746bcf5011d18ebad81ce24bf7c7068f5d0fb
nandani <reach.nandanipaliwal@gmail.com>
2024-01-23 00:04:47 +0530

    fix: cta-section fix

--- src/Components/cta-section/index.jsx
+++ src/Components/cta-section/index.jsx
@@ -17,30 +17,7 @@ export default function CtaSection() {
             </pattern>
           </defs>
         </svg>
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          stroke-width="0"
-          viewBox="0 0 16 16"
-          class="absolute left-2 top-1/2 z-20 h-9 w-9 -translate-y-1/2 text-gray-700 hover:text-gray-800 md:hidden "
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"></path>
-        </svg>
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          stroke-width="0"
-          viewBox="0 0 16 16"
-          class="absolute right-2 top-1/2 z-20 h-9 w-9 -translate-y-1/2 text-gray-700 hover:text-gray-800 md:hidden "
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path>
-        </svg>
+      
       </div>
       <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8 relative z-20">
         <h2 className="text-5xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
