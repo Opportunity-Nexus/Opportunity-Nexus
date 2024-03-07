d13763ca22682b2dc567dce20a51c5462a6380fa
Kingaman <amankag2626@gmail.com>
2024-03-07 22:43:36 +0530

    Allignment of authencation pages

--- src/Components/Authentication/SignupForm.jsx
+++ src/Components/Authentication/SignupForm.jsx
@@ -5,232 +5,235 @@ import InputOtp from "../../Pages/Authentication/InputOtp";
 import { useDispatch } from "react-redux";
 import { sendOtp } from "../../Services/Operations/AuthenticationApi";
 import { setSignupData } from "../../Redux/Slices/AuthSlice";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
-	const dispatch = useDispatch();
-	const accountType = "Student";
-	const [enrollmentNumber, setEnrollmentNumber] = useState("");
-	const [showOTPModel, setShowOTPModel] = useState(false);
-	const handleOnClose = () => {
-		setShowOTPModel(false);
-	};
+  const dispatch = useDispatch();
+  const accountType = "Student";
+  const [enrollmentNumber, setEnrollmentNumber] = useState("");
+  const [showOTPModel, setShowOTPModel] = useState(false);
+  const handleOnClose = () => {
+    setShowOTPModel(false);
+  };
 
-	const [formData, setFormData] = useState({
-		firstName: "",
-		lastName: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-		enrollmentNumber: "",
-	});
-	console.log(formData);
-	function changeHandler(event) {
-		const { name, value } = event.target;
-		if (name === "enrollmentNumber") {
-			setEnrollmentNumber(value);
-		}
-		setFormData((prev) => ({
-			...prev,
-			[name]: value,
-		}));
-	}
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    enrollmentNumber: "",
+  });
+  console.log(formData);
+  function changeHandler(event) {
+    const { name, value } = event.target;
+    if (name === "enrollmentNumber") {
+      setEnrollmentNumber(value);
+    }
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
-	const [showPassword, setShowPassword] = useState({
-		password: false,
-		confirmPassword: false,
-	});
+  const [showPassword, setShowPassword] = useState({
+    password: false,
+    confirmPassword: false,
+  });
 
-	const handleClick = (buttonName) => {
-		setShowPassword({
-			...showPassword,
-			[buttonName]: !showPassword[buttonName],
-		});
-	};
+  const handleClick = (buttonName) => {
+    setShowPassword({
+      ...showPassword,
+      [buttonName]: !showPassword[buttonName],
+    });
+  };
 
-	function submitHandler(event) {
-		event.preventDefault();
-		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-		if (!emailRegex.test(formData.email)) {
-			toast.error("Please enter a valid email address");
-			return;
-		}
-		if (formData.password.length < 8) {
-			toast.error("Password must be at least 8 characters long");
-			return;
-		}
-		if (formData.password !== formData.confirmPassword) {
-			toast.error("Passwords do not match");
-			return;
-		}
-		if (!/^BU\d{12}$/.test(enrollmentNumber)) {
-			toast.error("Please enter a valid enrollment number");
-			return;
-		}
-		if (formData.password !== formData.confirmPassword) {
-			toast.error("Passwords do not match!");
-			return;
-		}
-		const accountData = {
-			...formData,
-		};
-		const signupData = {
-			...accountData,
-			accountType,
-		};
-		// To be used after otp verification
-		dispatch(setSignupData(signupData));
-		dispatch(sendOtp(formData.email, setShowOTPModel));
-		console.log("Printing Final Account Data...");
-		console.log("Total SignUp Data:", JSON.stringify(signupData));
-	}
-	return (
-		<div className="mt-0">
-			<form
-				onSubmit={submitHandler}
-				className="flex flex-col w-full gap-y-2 mt-3"
-			>
-				<div className="flex gap-x-4">
-					<label className="w-full">
-						<p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
-							First Name<sup className="text-pink-200">*</sup>
-						</p>
-						<input
-							className="border border-gray-3 text-black dark:text-richblack-5  bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px] 
+  function submitHandler(event) {
+    event.preventDefault();
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(formData.email)) {
+      toast.error("Please enter a valid email address");
+      return;
+    }
+    if (formData.password.length < 8) {
+      toast.error("Password must be at least 8 characters long");
+      return;
+    }
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+    if (!/^BU\d{12}$/.test(enrollmentNumber)) {
+      toast.error("Please enter a valid enrollment number");
+      return;
+    }
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Passwords do not match!");
+      return;
+    }
+    const accountData = {
+      ...formData,
+    };
+    const signupData = {
+      ...accountData,
+      accountType,
+    };
+    // To be used after otp verification
+    dispatch(setSignupData(signupData));
+    dispatch(sendOtp(formData.email, setShowOTPModel));
+    console.log("Printing Final Account Data...");
+    console.log("Total SignUp Data:", JSON.stringify(signupData));
+  }
+  return (
+    <>
+      <div className="mt-1 ">
+        <form
+          onSubmit={submitHandler}
+          className="flex flex-col  w-full gap-y-2 mt-3"
+        >
+          <div className="flex gap-x-4">
+            <label className="w-full">
+              <p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
+                First Name<sup className="text-pink-200">*</sup>
+              </p>
+              <input
+                className="border border-gray-3 text-black dark:text-richblack-5  bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px] 
               "
-							required
-							type="text"
-							name="firstName"
-							id="firstName"
-							onChange={changeHandler}
-							value={formData.firstName}
-							placeholder="Enter first name"
-						/>
-					</label>
+                required
+                type="text"
+                name="firstName"
+                id="firstName"
+                onChange={changeHandler}
+                value={formData.firstName}
+                placeholder="Enter first name"
+              />
+            </label>
 
-					<label className="w-full">
-						<p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
-							Last Name<sup className="text-pink-200">*</sup>
-						</p>
-						<input
-							className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
-							required
-							type="text"
-							name="lastName"
-							id="lastName"
-							onChange={changeHandler}
-							value={formData.lastName}
-							placeholder="Enter last name"
-						/>
-					</label>
-				</div>
-				<label className="w-full">
-					<p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
-						Email Address<sup className="text-pink-200">*</sup>
-					</p>
-					<input
-						className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
-						required
-						type="email"
-						name="email"
-						id="email"
-						value={formData.email}
-						placeholder="Enter email address"
-						onChange={changeHandler}
-					/>
-				</label>
-				<div className="flex-col gap-x-4  ">
-					<label className="w-full relative ">
-						<p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
-							Create Password<sup className="text-pink-200">*</sup>
-						</p>
-						<input
-							className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
-							required
-							type={showPassword.password ? "text" : "password"}
-							name="password"
-							id="password"
-							onChange={changeHandler}
-							value={formData.password}
-							placeholder="Enter Password"
-						/>
+            <label className="w-full">
+              <p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
+                Last Name<sup className="text-pink-200">*</sup>
+              </p>
+              <input
+                className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
+                required
+                type="text"
+                name="lastName"
+                id="lastName"
+                onChange={changeHandler}
+                value={formData.lastName}
+                placeholder="Enter last name"
+              />
+            </label>
+          </div>
+          <label className="w-full">
+            <p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
+              Email Address<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
+              required
+              type="email"
+              name="email"
+              id="email"
+              value={formData.email}
+              placeholder="Enter email address"
+              onChange={changeHandler}
+            />
+          </label>
+          <div className="flex-col gap-x-4  ">
+            <label className="w-full relative ">
+              <p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem]">
+                Create Password<sup className="text-pink-200">*</sup>
+              </p>
+              <input
+                className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
+                required
+                type={showPassword.password ? "text" : "password"}
+                name="password"
+                id="password"
+                onChange={changeHandler}
+                value={formData.password}
+                placeholder="Enter Password"
+              />
 
-						<span
-							className=" absolute top-[38px] right-3  cursor-pointer"
-							onClick={() => handleClick("password")}
-						>
-							{showPassword.password ? (
-								<AiOutlineEye />
-							) : (
-								<AiOutlineEyeInvisible />
-							)}
-						</span>
-					</label>
+              <span
+                className=" absolute top-[38px] right-3  cursor-pointer"
+                onClick={() => handleClick("password")}
+              >
+                {showPassword.password ? (
+                  <AiOutlineEye />
+                ) : (
+                  <AiOutlineEyeInvisible />
+                )}
+              </span>
+            </label>
 
-					<label className="w-full relative ">
-						<p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem] mt-3">
-							Create Password<sup className="text-pink-200">*</sup>
-						</p>
-						<input
-							className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
-							required
-							type={showPassword.confirmPassword ? "text" : "password"}
-							name="confirmPassword"
-							id="confirmPassword"
-							onChange={changeHandler}
-							value={formData.confirmPassword}
-							placeholder="Confirm Password"
-						/>
+            <label className="w-full relative ">
+              <p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem] mt-3">
+                Create Password<sup className="text-pink-200">*</sup>
+              </p>
+              <input
+                className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
+                required
+                type={showPassword.confirmPassword ? "text" : "password"}
+                name="confirmPassword"
+                id="confirmPassword"
+                onChange={changeHandler}
+                value={formData.confirmPassword}
+                placeholder="Confirm Password"
+              />
 
-						<span
-							className="  absolute top-[82px] right-3  cursor-pointer"
-							onClick={() => handleClick("confirmPassword")}
-						>
-							{showPassword.confirmPassword ? (
-								<AiOutlineEye />
-							) : (
-								<AiOutlineEyeInvisible />
-							)}
-						</span>
-					</label>
-				</div>
-				<label>
-					<p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem] mt-3">
-						Enrollment No<sup className="text-pink-200">*</sup>
-					</p>
-					<input
-						className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
-						required
-						placeholder="BUxxxxxxxxxx"
-						value={formData.enrollmentNumber}
-						onChange={changeHandler}
-						name="enrollmentNumber"
-					/>
-				</label>
-				<button className=" bg-primary-600 hover:bg-primary-700 text-white  font-semibold px-[12px] rounded-[8px] py-[8px] mt-2">
-					Create Account
-				</button>
-				<p className="text-sm font-light text-black dark:text-white">
-					Already have an account?{" "}
-					<Link
-						to="/login"
-						className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-					>
-						Login here
-					</Link>
-				</p>
-			</form>
-			<InputOtp
-				email={formData.email}
-				onClose={handleOnClose}
-				visible={showOTPModel}
-				data={{
-					...formData,
-					accountType,
-				}}
-			/>
-		</div>
-	);
+              <span
+                className="  absolute top-[82px] right-3  cursor-pointer"
+                onClick={() => handleClick("confirmPassword")}
+              >
+                {showPassword.confirmPassword ? (
+                  <AiOutlineEye />
+                ) : (
+                  <AiOutlineEyeInvisible />
+                )}
+              </span>
+            </label>
+          </div>
+          <label>
+            <p className="text-black dark:text-richblack-5 mb-1 text-[0.875rem] leading-[1.375rem] mt-3">
+              Enrollment No<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              className="border border-gray-300 text-black dark:text-richblack-5 bg-gray-50 dark:bg-richblack-800 dark:border-none rounded-[8px] w-full px-[12px] py-[8px]"
+              required
+              placeholder="BUxxxxxxxxxx"
+              value={formData.enrollmentNumber}
+              onChange={changeHandler}
+              name="enrollmentNumber"
+            />
+          </label>
+          <button className=" bg-primary-600 hover:bg-primary-700 text-white  font-semibold px-[12px] rounded-[8px] py-[8px] mt-2">
+            Create Account
+          </button>
+          <p className="text-sm font-light text-black dark:text-white">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+            >
+              Login here
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <InputOtp
+        email={formData.email}
+        onClose={handleOnClose}
+        visible={showOTPModel}
+        data={{
+          ...formData,
+          accountType,
+        }}
+      />
+    </>
+  );
 };
 
 export default SignupForm;
--- src/Pages/Authentication/InputOtp.jsx
+++ src/Pages/Authentication/InputOtp.jsx
@@ -6,127 +6,127 @@ import { signUp } from "../../Services/Operations/AuthenticationApi";
 import { sendOtp } from "../../Services/Operations/AuthenticationApi";
 
 export default function InputOtp({ visible, onClose, email }) {
-	const { signupData } = useSelector((state) => state.auth);
-	const [otp, setOtp] = useState("");
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-	const handleOnClose = (e) => {
-		if (e.target.id === "cotainer") onClose();
-	};
-	console.log(
-		"Total account data recieved on OTP page :" + JSON.stringify(signupData)
-	);
+  const { signupData } = useSelector((state) => state.auth);
+  const [otp, setOtp] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleOnClose = (e) => {
+    if (e.target.id === "cotainer") onClose();
+  };
+  console.log(
+    "Total account data recieved on OTP page :" + JSON.stringify(signupData)
+  );
 
-	useEffect(() => {
-		// Only allow access of this route when user has filled the signup form
-		if (!signupData) {
-			navigate("/signup");
-		}
-		// eslint-disable-next-line
-	}, []);
+  useEffect(() => {
+    // Only allow access of this route when user has filled the signup form
+    if (!signupData) {
+      navigate("/signup");
+    }
+    // eslint-disable-next-line
+  }, []);
 
-	const handleOnSubmit = (e) => {
-		e.preventDefault();
-		const {
-			accountType,
-			firstName,
-			lastName,
-			email,
-			password,
-			confirmPassword,
-			enrollmentNumber,
-		} = signupData;
-		console.log(
-			"Total account data recieved on submitting otp here:" +
-				JSON.stringify(signupData)
-		);
-		dispatch(
-			signUp(
-				accountType,
-				firstName,
-				lastName,
-				email,
-				password,
-				confirmPassword,
-				otp,
-				enrollmentNumber,
-				navigate
-			)
-		);
-		console.log("I came here");
-	};
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const {
+      accountType,
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      enrollmentNumber,
+    } = signupData;
+    console.log(
+      "Total account data recieved on submitting otp here:" +
+        JSON.stringify(signupData)
+    );
+    dispatch(
+      signUp(
+        accountType,
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+        otp,
+        enrollmentNumber,
+        navigate
+      )
+    );
+    console.log("I came here");
+  };
 
-	if (!visible) return null;
-	return (
-		<div
-			id="cotainer"
-			onClick={handleOnClose}
-			className=" flex min-h-screen flex-col justify-center overflow-hidden py-12 
+  if (!visible) return null;
+  return (
+    <div
+      id="cotainer"
+      onClick={handleOnClose}
+      className=" flex min-h-screen flex-col justify-center overflow-hidden py-12 
       fixed inset-0  bg-black bg-opacity-30 backdrop-blur-sm"
-		>
-			<div className="relative bg-white dark:bg-richblack-900   px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
-				<div className="mx-auto flex w-full max-w-md flex-col space-y-16">
-					<div className="flex flex-col items-center justify-center text-center space-y-2">
-						<div className="font-semibold text-3xl text-black dark:text-white">
-							<p>Email Verification</p>
-						</div>
-						<div className="flex flex-row text-sm font-medium text-black dark:text-white">
-							<p>Enter the OTP you received at your registered email {email}</p>
-						</div>
-					</div>
+    >
+      <div className="relative bg-white dark:bg-richblack-900   px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
+        <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+          <div className="flex flex-col items-center justify-center text-center space-y-2">
+            <div className="font-semibold text-3xl text-black dark:text-white">
+              <p>Email Verification</p>
+            </div>
+            <div className="flex flex-row text-sm font-medium text-black dark:text-white">
+              <p>Enter the OTP you received at your registered email {email}</p>
+            </div>
+          </div>
 
-					<div>
-						<form onSubmit={handleOnSubmit}>
-							<div className="flex flex-col space-y-16">
-								<div className="flex justify-center ">
-									<OtpInput
-										value={otp}
-										onChange={setOtp}
-										numInputs={6}
-										renderInput={(props) => (
-											<input
-												{...props}
-												placeholder="-"
-												style={{
-													boxShadow:
-														"inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
-												}}
-												className="w-[48px] lg:w-[60px] border-0 bg-richblack-25 dark:bg-richblack-800 rounded-[0.5rem] text-black dark:text-richblack-5 aspect-square text-center focus:border-0 focus:outline-2 focus:outline-yellow-50"
-											/>
-										)}
-										containerStyle={{
-											justifyContent: "space-between",
-											gap: "0 6px",
-										}}
-									/>
-								</div>
+          <div>
+            <form onSubmit={handleOnSubmit}>
+              <div className="flex flex-col space-y-16">
+                <div className="flex justify-center ">
+                  <OtpInput
+                    value={otp}
+                    onChange={setOtp}
+                    numInputs={6}
+                    renderInput={(props) => (
+                      <input
+                        {...props}
+                        placeholder="-"
+                        style={{
+                          boxShadow:
+                            "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                        }}
+                        className="w-[48px] lg:w-[60px] border-0 bg-richblack-25 dark:bg-richblack-800 rounded-[0.5rem] text-black dark:text-richblack-5 aspect-square text-center focus:border-0 focus:outline-2 focus:outline-yellow-50"
+                      />
+                    )}
+                    containerStyle={{
+                      justifyContent: "space-between",
+                      gap: "0 6px",
+                    }}
+                  />
+                </div>
 
-								<div className="flex flex-col space-y-5">
-									<div>
-										<button
-											type="submit"
-											className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-blue-700 border-none text-white text-xl shadow-sm"
-										>
-											Verify Account
-										</button>
-									</div>
-								</div>
-							</div>
-						</form>
-						<div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
-								<p>Didn't recieve code?</p>{" "}
-								<button
-									onClick={() => dispatch(sendOtp(signupData.email, navigate))}
-									className="flex flex-row items-center text-blue-600"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									Resend
-								</button>
-							</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                <div className="flex flex-col space-y-5">
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-blue-700 border-none text-white text-xl shadow-sm"
+                    >
+                      Verify Account
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
+              <p>Didn't recieve code?</p>{" "}
+              <button
+                onClick={() => dispatch(sendOtp(signupData.email, navigate))}
+                className="flex flex-row items-center text-blue-600"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resend
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
