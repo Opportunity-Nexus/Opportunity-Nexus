0b2445184aa012a2598339597556b38ff1a42197
nandani <reach.nandanipaliwal@gmail.com>
2024-03-16 21:44:19 +0530

    fix: text colour of input field of search
    Signed-off-by: nandani <reach.nandanipaliwal@gmail.com>

--- src/Common/ApplicationSidebar.jsx
+++ src/Common/ApplicationSidebar.jsx
@@ -219,13 +219,13 @@ export default function ApplicationLayoutSidebar() {
                   <label htmlFor="search-field" className="sr-only">
                     Search
                   </label>
-                  <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+                  <div className="relative w-full text-black  dark:text-white focus-within:text-gray-600">
                     <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
                       <SearchIcon className="h-5 w-5" aria-hidden="true" />
                     </div>
                     <input
                       id="search-field"
-                      className="block w-full h-full pl-8 pr-3 py-2 border-transparent bg-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+                      className="block w-full h-full pl-8 pr-3 py-2 border-transparent bg-transparent text-black  dark:text-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
                       placeholder="Search"
                       type="search"
                       name="search"
@@ -236,7 +236,7 @@ export default function ApplicationLayoutSidebar() {
               <div className="ml-4 flex items-center md:ml-6 gap-3">
                 <button
                   type="button"
-                  className="bg-white  dark:bg-gray-900 p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                  className="bg-white  dark:bg-gray-900 p-1 rounded-full text-black  dark:text-white hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
                 >
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
