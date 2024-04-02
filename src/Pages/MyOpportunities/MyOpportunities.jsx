6baf920e7d452dfd01106964f89f3a7f9682fcf2
nandani-paliwal <reach.nandanipalwail@gmail.com>
2024-04-03 00:10:39 +0530

    fix: loading screen before state load for saved opportunities
    Signed-off-by: nandani-paliwal <reach.nandanipalwail@gmail.com>

--- src/Pages/MyOpportunities/MyOpportunities.jsx
+++ src/Pages/MyOpportunities/MyOpportunities.jsx
@@ -11,8 +11,8 @@ const MyOpportunities = () => {
    * @type {string} - offCampus | onCampus
    */
   const [opportunityType, setOpportunityType] = useState("offCampus");
-  const [savedOpportunitiesList, setSavedOpportunitiesList] = useState([]);
-  const {token} = useSelector((state)=>state.auth);
+  const [savedOpportunitiesList, setSavedOpportunitiesList] = useState(null);
+  const { token } = useSelector((state) => state.auth);
 
   //----------------------PAGINTAION----------------------//
 
@@ -36,47 +36,46 @@ const MyOpportunities = () => {
     ? paginationMeta.totalResults / pageSize
     : 1;
 
-    useEffect(() => {
-      async function getSavedOpportunities() {
-    
-        if (token) {
-          try {
-            const response = await fetch(offCampusEndpoints.GET_ALL_BOOKMARK_OPPORTUNITY, {
-              method: 'GET',
+  useEffect(() => {
+    async function getSavedOpportunities() {
+      if (token) {
+        try {
+          const response = await fetch(
+            offCampusEndpoints.GET_ALL_BOOKMARK_OPPORTUNITY,
+            {
+              method: "GET",
               headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-              }
-            });
-    
-            const data = await response.json();
-            if (response.ok) {
-              console.log('Success:', data);
-              setSavedOpportunitiesList(() => {return data.data});
-    
-              // setPaginationMeta(data.pagination);
-            } 
-            
-            else {
-              // If we get an HTTP error response
-              console.error('Fetch error:', data.message);
-              // Potentially show a toast notification with the error
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+              },
             }
-          } catch (error) {
-            console.error('Request failed:', error);
-            // Handle network errors or other unexpected errors
+          );
+
+          const data = await response.json();
+          if (response.ok) {
+            console.log("Success:", data);
+            setSavedOpportunitiesList(() => {
+              return data.data;
+            });
+
+            // setPaginationMeta(data.pagination);
+          } else {
+            // If we get an HTTP error response
+            console.error("Fetch error:", data.message);
+            // Potentially show a toast notification with the error
           }
-        } 
-        
-        else {
-          console.error('Token is undefined or not found');
-          // Potentially handle the lack of a token, like redirecting to a login page
+        } catch (error) {
+          console.error("Request failed:", error);
+          // Handle network errors or other unexpected errors
         }
+      } else {
+        console.error("Token is undefined or not found");
+        // Potentially handle the lack of a token, like redirecting to a login page
       }
-   
-      getSavedOpportunities();
-    }, [paginationMeta, token, opportunityType]); // Depend on paginationMeta and opportunityType
-   
+    }
+
+    getSavedOpportunities();
+  }, [paginationMeta, token, opportunityType]); // Depend on paginationMeta and opportunityType
 
   return (
     <>
@@ -105,81 +104,89 @@ const MyOpportunities = () => {
           </select>
         </div>
 
-        {savedOpportunitiesList.length === 0 ? (
-          <div className="flex flex-col justify-center items-center lg:w-2/4 flex-1 dark:text-white mx-auto self-center">
-            <img src={OpportunitiesNotFoundImg} alt="OppNotFound" />
-            <h2 className="text-2xl sm:text-3xl font-bold animate-bounce text-center ">
-              You have not saved any opportunities yet!
-            </h2>
-          </div>
-        ) : (
+        {savedOpportunitiesList ? (
           <>
-            <div className="flex flex-wrap justify-center mx-auto mt-4 mb-7 flex-1">
-              {savedOpportunitiesList.map((opportunity, index) => (
-                <SavedOpportunityCard key={index} {...opportunity} />
-              ))}
-            </div>
-
-            {/* ------------PAGINATION----------- */}
-            <div className="flex items-center justify-center w-2/4 mt-8">
-              <button
-                className={`flex items-center text-black dark:text-white rounded-lg p-3  ${
-                  paginationMeta.currentPage <= 1
-                    ? "cursor-not-allowed text-gray-400 "
-                    : "cursor-pointer"
-                }`}
-                onClick={() =>
-                  setPaginationMeta((data) => ({
-                    ...data,
-                    currentPage: data.currentPage - 1,
-                  }))
-                }
-                disabled={paginationMeta.currentPage <= 1}
-              >
-                <TbPlayerTrackPrevFilled />
-                <span className="uppercase">previous</span>
-              </button>
-              <div className="flex items-center justify-center">
-                {Array(totalPages)
-                  .fill(null)
-                  .map((_, index) => (
-                    <button
-                      key={index + 1}
-                      className={
-                        "border rounded-full px-4 py-2 ml-3 mr-3 dark:text-white focus:ring-2 "
-                      }
-                      onClick={() =>
-                        setPaginationMeta((data) => ({
-                          ...data,
-                          currentPage: index + 1,
-                        }))
-                      }
-                    >
-                      {index + 1}
-                    </button>
-                  ))}
+            {savedOpportunitiesList.length === 0 ? (
+              <div className="flex flex-col justify-center items-center lg:w-2/4 flex-1 dark:text-white mx-auto self-center">
+                <img src={OpportunitiesNotFoundImg} alt="OppNotFound" />
+                <h2 className="text-2xl sm:text-3xl font-bold animate-bounce text-center ">
+                  You have not saved any opportunities yet!
+                </h2>
               </div>
-              <button
-                className={`flex items-center text-black dark:text-white rounded-lg p-3  ${
-                  paginationMeta.currentPage >= paginationMeta.totalResults
-                    ? "cursor-not-allowed text-gray-400"
-                    : "cursor-pointer"
-                }`}
-                onClick={() =>
-                  setPaginationMeta((data) => ({
-                    ...data,
-                    currentPage: data.currentPage + 1,
-                  }))
-                }
-                disabled={
-                  paginationMeta.currentPage >= paginationMeta.totalResults
-                }
-              >
-                <span className="uppercase ml-3">next</span>
-                <TbPlayerTrackNextFilled />
-              </button>
-            </div>
+            ) : (
+              <>
+                <div className="flex flex-wrap justify-center mx-auto mt-4 mb-7 flex-1">
+                  {savedOpportunitiesList.map((opportunity, index) => (
+                    <SavedOpportunityCard key={index} {...opportunity} />
+                  ))}
+                </div>
+
+                {/* ------------PAGINATION----------- */}
+                <div className="flex items-center justify-center w-2/4 mt-8">
+                  <button
+                    className={`flex items-center text-black dark:text-white rounded-lg p-3  ${
+                      paginationMeta.currentPage <= 1
+                        ? "cursor-not-allowed text-gray-400 "
+                        : "cursor-pointer"
+                    }`}
+                    onClick={() =>
+                      setPaginationMeta((data) => ({
+                        ...data,
+                        currentPage: data.currentPage - 1,
+                      }))
+                    }
+                    disabled={paginationMeta.currentPage <= 1}
+                  >
+                    <TbPlayerTrackPrevFilled />
+                    <span className="uppercase">previous</span>
+                  </button>
+                  <div className="flex items-center justify-center">
+                    {Array(totalPages)
+                      .fill(null)
+                      .map((_, index) => (
+                        <button
+                          key={index + 1}
+                          className={
+                            "border rounded-full px-4 py-2 ml-3 mr-3 dark:text-white focus:ring-2 "
+                          }
+                          onClick={() =>
+                            setPaginationMeta((data) => ({
+                              ...data,
+                              currentPage: index + 1,
+                            }))
+                          }
+                        >
+                          {index + 1}
+                        </button>
+                      ))}
+                  </div>
+                  <button
+                    className={`flex items-center text-black dark:text-white rounded-lg p-3  ${
+                      paginationMeta.currentPage >= paginationMeta.totalResults
+                        ? "cursor-not-allowed text-gray-400"
+                        : "cursor-pointer"
+                    }`}
+                    onClick={() =>
+                      setPaginationMeta((data) => ({
+                        ...data,
+                        currentPage: data.currentPage + 1,
+                      }))
+                    }
+                    disabled={
+                      paginationMeta.currentPage >= paginationMeta.totalResults
+                    }
+                  >
+                    <span className="uppercase ml-3">next</span>
+                    <TbPlayerTrackNextFilled />
+                  </button>
+                </div>
+              </>
+            )}
           </>
+        ) : (
+          <div className="h-screen flex flex-1 items-center justify-center">
+            <div className="loader"></div>
+          </div>
         )}
       </div>
     </>
