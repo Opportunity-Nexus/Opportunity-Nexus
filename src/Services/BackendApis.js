e5e29b9172aef3f160e65231e6ff98f88cbacedc
Sheetal <sheetaltyagichd@gmail.com>
2024-04-14 19:22:55 +0530

    Fixed:BASE_URL

--- src/Services/BackendApis.js
+++ src/Services/BackendApis.js
@@ -1,6 +1,6 @@
-const BASE_URL = "http://localhost:4000/api/v1";
+// const BASE_URL = "http://localhost:4000/api/v1";
 // const BASE_URL = "https://api.hostellelo.in/api/v1";
-// const BASE_URL = "https://opportunity-nexus-backend.onrender.com/api/v1";
+const BASE_URL = "https://opportunity-nexus-backend.onrender.com/api/v1";
 
 // AUTH ENDPOINTS
 export const authEndpoints = {
